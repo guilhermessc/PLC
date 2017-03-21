439cdbbe1a034ac56b5bb61d1f8824db77e48537
Guilherme Sales Santa Cruz <gssc@WINDOWS.CIN.UFPE.BR>
2017-03-21 09:32:38 -0300

    Remove cfcv f function (not working)

--- gssc.hs
+++ gssc.hs
@@ -3,8 +3,13 @@ mirror :: [t] -> [t]
 mirror [] = []
 mirror (a:as) = [a] ++ mirror as ++ [a]
 
--- teste
-f :: [Int] -> [Int]
-f [] = 0
-f [x:xs] = x + f xs
-
+-- This function creates a list of n elements
+lonel :: Int -> t -> [t]
+lonel 0 e = []
+lonel n e | n < 0 = error "Invalid argument"
+          | otherwise = [e] ++ (lonel (n-1) e)
+            
+-- This function repeats each element of list n times
+rep :: Int -> [t] -> [t]
+rep n [] = []
+rep n (a:as) = (lonel n a) ++ (rep n as)
