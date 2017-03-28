e0c4812525cc0d1f419bdba3855cba11e4e456c3
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-03-28 08:54:23 -0300

    UPDATE 1st quest/ list 1 . improve reduz_1 function algorithm

--- gssc/1_quest.hs
+++ gssc/1_quest.hs
@@ -7,6 +7,6 @@ qsort [] = []
 qsort (a:as) = qsort [x |x <- as, x < a] ++ [x | x <- (a:as), x == a] ++ qsort [x | x <- as, a < x]
 
 reduz_1 :: [Int] -> [Int]
-reduz_1 (a:as) = z \\ [(head z)..(last z)]
-               where z = qsort[x | x <- (a:as), [y | y <- (a:as), y == x] /= [x]]
+reduz_1 a = z \\ [(head z)..(last z)]
+               where z = qsort a
 
