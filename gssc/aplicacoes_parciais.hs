c7d7f6fa1879282a28ac387496ad1e0af8f59131
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-04-04 09:20:01 -0300

    Add examples from cmd line in comment

--- gssc/aplicacoes_parciais.hs
+++ gssc/aplicacoes_parciais.hs
@@ -7,3 +7,9 @@ m2 = multiply 2
 iter :: Int -> (t -> t) -> t -> t
 iter 0 f = id
 iter n f = f . (iter (n-1) f)
+
+-- examples from cmd line ----------------------------
+-- iter 10 m2 3
+-- iter 10 (*2) 3
+-- iter 10 (`div` 2) 30000
+------------------------------------------------------
