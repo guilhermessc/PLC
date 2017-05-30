6c68e164526d650b838f71cbdf16bac2c8b8edd6
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-05-30 09:18:10 -0300

    First java project

--- gssc/lista_1/2_quest.hs
+++
@@ -1,9 +0,0 @@
--- 2a questão
-
-size_list :: [Int] -> Int
-size_list [] = 0
-size_list (a:as) = 1 + size_list(as)
-
-tupla_q :: [Int] -> [(Int, Int)]
-tupla_q [] = []
-tupla_q (a:as) = tupla_q [ x | x <- as, x < a] ++ [(a, (size_list [x | x <- (a:as), x == a]))] ++ tupla_q [ x | x <- as, a < x]
